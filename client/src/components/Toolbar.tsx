5323dbd0d5f357f31bd787bf8c0266902e275107
talucre <talucreee@gmail.com>
2026-03-10 18:34:28 +0300

    toolbar buttons with icons

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -1,7 +1,19 @@
 import '@/styles/toolbar.scss'
 
 const Toolbar = () => {
-    return <div className="toolbar"></div>
+    return (
+        <div className="toolbar">
+            <button className="toolbar__btn brush"></button>
+            <button className="toolbar__btn rect"></button>
+            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn eraser"></button>
+            <button className="toolbar__btn line"></button>
+            <input type="color" className="color-picker" />
+            <button className="toolbar__btn undo"></button>
+            <button className="toolbar__btn redo"></button>
+            <button className="toolbar__btn save"></button>
+        </div>
+    )
 }
 
 export default Toolbar
